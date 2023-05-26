2bb2e910ba31e5cb5f0eebe2e7a7e4d1de6ae91f
Franksu <fsuriel@gmail.com>
2023-05-26 19:06:34 -0400

    added form to contact me

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,7 +1,10 @@
 // import react and icons
-import React from "react";
+import React, { useState, useRef } from "react";
+import emailjs from "@emailjs/browser"
+import { validateEmail } from "../utils/helpers";
 import githubIcon from './pictures/github-original.svg'
 import linkedin from './pictures/linkedin-original.svg'
+
 
 // set icon size and section height
 const styles = {
@@ -9,25 +12,119 @@ const styles = {
         width: '100px'
     },
     height: {
-        height: '300px'
+        height: '200px'
     }
 }
 
-// create a function to manage contact me section
-// set the title and create links to my github and linkedin
 function ContactMe() {
+    const [email, setEmail] = useState("");
+    const [fullName, setFullName] = useState("");
+    const [message, setMessage] = useState("");
+    const [errorMessage, setErrorMessage] = useState("");
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+
+        if (!validateEmail(email) || !fullName || !message) {
+            setErrorMessage("Email, name, or message is not valid");
+            return;
+        }
+
+        emailjs.sendForm(
+            'service_bhy996m',
+            'template_vxsdzf9',
+            form.current,
+            '6Pa3MRGA0UbxP5RKe'
+        )
+            .then((result) => {
+                console.log(result.text);
+                setEmail("");
+                setFullName("");
+                setMessage("");
+            }, (error) => {
+                console.log(error.text);
+            });
+    };
+
+    const handleInputChange = (e) => {
+        const { target } = e;
+        const inputType = target.name;
+        const inputValue = target.value;
+
+        if (inputType === "email") {
+            setEmail(inputValue);
+        } else if (inputType === "fullName") {
+            setFullName(inputValue);
+        } else {
+            setMessage(inputValue)
+        }
+
+        if (inputValue.trim() === "") {
+            setErrorMessage(`${inputType} is required`)
+        } else {
+            setErrorMessage('')
+        }
+    };
+
+    // create a function to manage contact me section
+    // set the title and create links to my github and linkedin
     return (
-        <footer id="contactMe" className="row p-5" style={styles.height}>
+        <div id="contactMe">
             <h1 className="text-center pb-5" >Contact Me</h1>
-            <div className=' d-flex justify-content-center'>
-                <a href="https://github.com/FranklynSuriel">
-                    <img src={githubIcon} alt="github icon" style={styles.icon} />
-                </a>
-                <a href="https://www.linkedin.com/in/franklynsuriel/">
-                    <img src={linkedin} alt="linkedin icon" style={styles.icon} />
-                </a>
+            <div className="container mb-3">
+                <div className="d-flex justify-content-center"  >
+                    <form ref={form} onSubmit={sendEmail} className="form mb-5 w-25">
+                        <input
+                            value={fullName}
+                            name="fullName"
+                            onChange={handleInputChange}
+                            onBlur={handleInputChange}
+                            type="text"
+                            placeholder="Your Name"
+                            className="form-control mb-3"
+                        />
+                        <input
+                            value={email}
+                            name="email"
+                            onChange={handleInputChange}
+                            onBlur={handleInputChange}
+                            type="email"
+                            placeholder="Your Email"
+                            className="form-control mb-3"
+                        />
+                        <textarea
+                            value={message}
+                            name="message"
+                            onChange={handleInputChange}
+                            onBlur={handleInputChange}
+                            type="textarea"
+                            placeholder="Message"
+                            className="form-control mb-3"
+                        />
+                        <button className="btn btn-primary" type="submit" >
+                            Submit
+                        </button>
+                    </form>
+                    {errorMessage && (
+                        <div>
+                            <p className="error-text">{errorMessage}</p>
+                        </div>
+                    )}
+                </div>
             </div>
-        </footer>
+            <footer className="row p-4 mt-5" style={styles.height}>
+                <div className=' d-flex justify-content-center'>
+                    <a href="https://github.com/FranklynSuriel">
+                        <img src={githubIcon} alt="github icon" style={styles.icon} />
+                    </a>
+                    <a href="https://www.linkedin.com/in/franklynsuriel/">
+                        <img src={linkedin} alt="linkedin icon" style={styles.icon} />
+                    </a>
+                </div>
+            </footer>
+        </div>
     )
 }
 
