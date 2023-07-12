248f34971080ae396eb917b59f8ee3c00448aa90
Franksu <fsuriel@gmail.com>
2023-07-11 22:11:55 -0400

    Added email and email icon, changed size for footer icons, add message on footer

--- src/components/contactMe.js
+++ src/components/contactMe.js
@@ -1,134 +1,147 @@
 // import react and icons
 import React, { useState, useRef } from "react";
-import emailjs from "@emailjs/browser"
+import emailjs from "@emailjs/browser";
 import { validateEmail } from "../utils/helpers";
-import githubIcon from './pictures/github-original.svg'
-import linkedin from './pictures/linkedin-original.svg'
-
+import githubIcon from "./pictures/github-original.svg";
+import linkedin from "./pictures/linkedin-original.svg";
+import mail from "./pictures/mail.svg";
 
 // set icon size and section height
 const styles = {
-    icon: {
-        width: '100px'
-    },
-    height: {
-        height: '200px'
-    }
-}
+  icon: {
+    width: "60px",
+  },
+  height: {
+    height: "200px",
+  },
+};
 
 function ContactMe() {
-    const [email, setEmail] = useState("");
-    const [fullName, setFullName] = useState("");
-    const [message, setMessage] = useState("");
-    const [errorMessage, setErrorMessage] = useState("");
+  const [email, setEmail] = useState("");
+  const [fullName, setFullName] = useState("");
+  const [message, setMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-    const form = useRef();
+  const form = useRef();
 
-    const sendEmail = (e) => {
-        e.preventDefault();
+  const sendEmail = (e) => {
+    e.preventDefault();
 
-        if (!validateEmail(email) || !fullName || !message) {
-            setErrorMessage("Email, name, or message is not valid");
-            return;
+    if (!validateEmail(email) || !fullName || !message) {
+      setErrorMessage("Email, name, or message is not valid");
+      return;
+    }
+
+    emailjs
+      .sendForm(
+        "service_bhy996m",
+        "template_vxsdzf9",
+        form.current,
+        "6Pa3MRGA0UbxP5RKe"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setEmail("");
+          setFullName("");
+          setMessage("");
+        },
+        (error) => {
+          console.log(error.text);
         }
+      );
+  };
 
-        emailjs.sendForm(
-            'service_bhy996m',
-            'template_vxsdzf9',
-            form.current,
-            '6Pa3MRGA0UbxP5RKe'
-        )
-            .then((result) => {
-                console.log(result.text);
-                setEmail("");
-                setFullName("");
-                setMessage("");
-            }, (error) => {
-                console.log(error.text);
-            });
-    };
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
 
-    const handleInputChange = (e) => {
-        const { target } = e;
-        const inputType = target.name;
-        const inputValue = target.value;
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "fullName") {
+      setFullName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
 
-        if (inputType === "email") {
-            setEmail(inputValue);
-        } else if (inputType === "fullName") {
-            setFullName(inputValue);
-        } else {
-            setMessage(inputValue)
-        }
+    if (inputValue.trim() === "") {
+      setErrorMessage(`${inputType} is required`);
+    } else {
+      setErrorMessage("");
+    }
+  };
 
-        if (inputValue.trim() === "") {
-            setErrorMessage(`${inputType} is required`)
-        } else {
-            setErrorMessage('')
-        }
-    };
-
-    // create a function to manage contact me section
-    // set the title and create links to my github and linkedin
-    return (
-        <div id="contactMe">
-            <h1 className="text-center pb-5" >Contact Me</h1>
-            <div className="container mb-3">
-                <div className="d-flex justify-content-center"  >
-                    <form ref={form} onSubmit={sendEmail} className="form mb-5 col-8 col-md-6 col-lg-5">
-                        <input
-                            value={fullName}
-                            name="fullName"
-                            onChange={handleInputChange}
-                            onBlur={handleInputChange}
-                            type="text"
-                            placeholder="Your Name"
-                            className="form-control mb-3"
-                            id="inputName"
-                        />
-                        <input
-                            value={email}
-                            name="email"
-                            onChange={handleInputChange}
-                            onBlur={handleInputChange}
-                            type="email"
-                            placeholder="Your Email"
-                            className="form-control mb-3"
-                            id="inputEmail"
-                        />
-                        <textarea
-                            value={message}
-                            name="message"
-                            onChange={handleInputChange}
-                            onBlur={handleInputChange}
-                            type="textarea"
-                            placeholder="Message"
-                            className="form-control mb-3"
-                            id="inputMessage"
-                        />
-                        <button className="btn btn-primary" type="submit" >
-                            Submit
-                        </button>
-                    </form>
-                    {errorMessage && (
-                        <div>
-                            <p className="error-text">{errorMessage}</p>
-                        </div>
-                    )}
-                </div>
+  // create a function to manage contact me section
+  // set the title and create links to my github and linkedin
+  return (
+    <div id="contactMe">
+      <h1 className="text-center">Contact Me</h1>
+      <h6 className="text-center pb-5">fsuriel@gmail.com</h6>
+      <div className="container mb-3">
+        <div className="d-flex justify-content-center">
+          <form
+            ref={form}
+            onSubmit={sendEmail}
+            className="form mb-5 col-8 col-md-6 col-lg-5"
+          >
+            <input
+              value={fullName}
+              name="fullName"
+              onChange={handleInputChange}
+              onBlur={handleInputChange}
+              type="text"
+              placeholder="Your Name"
+              className="form-control mb-3"
+              id="inputName"
+            />
+            <input
+              value={email}
+              name="email"
+              onChange={handleInputChange}
+              onBlur={handleInputChange}
+              type="email"
+              placeholder="Your Email"
+              className="form-control mb-3"
+              id="inputEmail"
+            />
+            <textarea
+              value={message}
+              name="message"
+              onChange={handleInputChange}
+              onBlur={handleInputChange}
+              type="textarea"
+              placeholder="Message"
+              className="form-control mb-3"
+              id="inputMessage"
+            />
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </form>
+          {errorMessage && (
+            <div>
+              <p className="error-text">{errorMessage}</p>
             </div>
-            <footer className="row p-4 mt-5" style={styles.height}>
-                <div className=' d-flex justify-content-center'>
-                    <a href="https://github.com/FranklynSuriel">
-                        <img src={githubIcon} alt="github icon" style={styles.icon} />
-                    </a>
-                    <a href="https://www.linkedin.com/in/franklynsuriel/">
-                        <img src={linkedin} alt="linkedin icon" style={styles.icon} />
-                    </a>
-                </div>
-            </footer>
+          )}
         </div>
-    )
+      </div>
+      <footer className="row p-4 mt-5" style={styles.height}>
+        <div className=" d-flex justify-content-center">
+          <a href="https://github.com/FranklynSuriel">
+            <img src={githubIcon} alt="github icon" style={styles.icon} />
+          </a>
+          <a href="https://www.linkedin.com/in/franklynsuriel/">
+            <img src={linkedin} alt="linkedin icon" style={styles.icon} />
+          </a>
+          <a href="mailto:fsuriel@gmail.com">
+            <img src={mail} alt="email icon" style={styles.icon} />
+          </a>
+        </div>
+        <h6 className="text-center my-5">Made with ❤️ by Franklyn</h6>
+      </footer>
+    </div>
+  );
 }
 
 // export ContactMe
