71069c9e7d1e7c7a0b84d7cdcb0edda328be3d9a
Franksu <fsuriel@gmail.com>
2023-07-23 23:41:40 -0400

    Created a file for the image and image-data

---
+++ src/components/portfolio/Cards.js
@@ -0,0 +1,67 @@
+import YearlyRecord from "../pictures/My_Yeraly_Record_Profile.png";
+import NeoPlants from "../pictures/Neo Plants.jpg";
+import MovieBattle from "../pictures/Movie_Battle.jpg";
+import NoteTaker from "../pictures/Note_Taker.jpg";
+import TechBlog from "../pictures/Tech Blog app.jpg";
+import WorkDayScheduler from "../pictures/Work_Day_Scheduler.jpg";
+import PasswordGenerator from "../pictures/password generator.jpg";
+
+export default [
+  {
+    title: "My Yearly Record",
+    text: "An easy way to create lists of your interested books or tv shows while making friends with common interest",
+    img: YearlyRecord,
+    alt: "My Yearly List app",
+    deployedLink: "https://gentle-falls-37134.herokuapp.com/",
+    githubLink: "https://github.com/FranklynSuriel/My_Yearly_Record",
+  },
+  {
+    title: "NeoPlants",
+    text: "An interactive game app where you can grow you plant on the screen",
+    img: NeoPlants,
+    alt: "NeoPlant game",
+    deployedLink: "https://grey-stones.herokuapp.com/",
+    githubLink:
+      "https://github.com/alexander1262/NeoPlants-Interactive-Web-Game",
+  },
+  {
+    title: "Movie Battle",
+    text: "Movie Battle is a fun app that lets two people play a trivia game to decide what movie to watch.",
+    img: MovieBattle,
+    alt: "Movie Battle app",
+    deployedLink: "https://franklynsuriel.github.io/movie-battle/",
+    githubLink: "https://github.com/FranklynSuriel/movie-battle",
+  },
+  {
+    title: "Note Taker",
+    text: "A note taker application to keep your notes save and easy organized.",
+    img: NoteTaker,
+    alt: "Note Taker app",
+    deployedLink: "https://thawing-everglades-51813.herokuapp.com/",
+    githubLink: "https://github.com/FranklynSuriel/Note_Taker",
+  },
+  {
+    title: "Tech_Blog",
+    text: "A CMS-style blog where you can publish articles, blog post, comment other user post, update or delete your post and more.",
+    img: TechBlog,
+    alt: "Tech Blog app",
+    deployedLink: "https://glacial-headland-83578.herokuapp.com/",
+    githubLink: "https://github.com/FranklynSuriel/Tech_Blog",
+  },
+  {
+    title: "Work_Day_Scheduler",
+    text: "Simple calendar application that allows a user to save events for each hour of the day.",
+    img: WorkDayScheduler,
+    alt: "Work_Day_Scheduler app",
+    deployedLink: "https://franklynsuriel.github.io/Work_Day_Scheduler/",
+    githubLink: "https://github.com/FranklynSuriel/Work_Day_Scheduler",
+  },
+  {
+    title: "Password Generator",
+    text: "A CMS-style blog where you can publish articles, blog post, comment other user post, update or delete your post and more.",
+    img: PasswordGenerator,
+    alt: "Password Generator app",
+    deployedLink: "https://franklynsuriel.github.io/Password_Generator/",
+    githubLink: "https://github.com/FranklynSuriel/Password_Generator",
+  },
+];
